d2e12fdf58b0f5f40df299b9f243c8bc90ce1d4d
miles <29894500+NostalgicLorikeet@users.noreply.github.com>
2026-03-12 23:24:26 -0400

    Update CardboardRecipes.java

--- src/main/java/nostalgic/cardboardboxes/loaders/recipes/CardboardRecipes.java
+++ src/main/java/nostalgic/cardboardboxes/loaders/recipes/CardboardRecipes.java
@@ -1,6 +1,15 @@
 package nostalgic.cardboardboxes.loaders.recipes;
 
+import gregtech.api.recipes.ModHandler;
+import gregtech.api.unification.OreDictUnifier;
+import gregtech.api.unification.material.Materials;
+import gregtech.api.unification.ore.OrePrefix;
+import gregtech.api.unification.stack.UnificationEntry;
 import gregtech.common.items.MetaItems;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import nostalgic.cardboardboxes.machine.CardboardMetaTileEntities;
+import nostalgic.cardboardboxes.materials.CardboardMaterials;
 
 import static gregtech.api.recipes.RecipeMaps.ASSEMBLER_RECIPES;
 import static gregtech.api.unification.material.Materials.Glue;
@@ -14,5 +23,16 @@ public class CardboardRecipes {
                 .outputs(CARDBOARD_CRATE.getStackForm()).duration(200).circuitMeta(1).buildAndRegister();
         ASSEMBLER_RECIPES.recipeBuilder().EUt(8).input(plate, Cardboard, 6).fluidInputs(Glue.getFluid(20))
                 .outputs(CARDBOARD_CRATE.getStackForm()).duration(200).circuitMeta(1).buildAndRegister();
+
+        ModHandler.addShapedRecipe(true, "cardboard_crate", CardboardMetaTileEntities.CARDBOARD_CRATE.getStackForm(),
+                "RRR", "RPR", " R ",
+                'R', new UnificationEntry(OrePrefix.plate, CardboardMaterials.Cardboard),
+                'P', MetaItems.STICKY_RESIN.getStackForm());
+
+        //ModHandler.addShapedRecipe(true, "cardboard_sheet", OreDictUnifier.get(OrePrefix.plate, CardboardMaterials.Cardboard),
+        //        "CRC", "CRC", "CBC",
+        //        'R', MetaItems.STICKY_RESIN.getStackForm(),
+        //        'B', new ItemStack(Items.WATER_BUCKET),
+        //        'C', OreDictUnifier.get(OrePrefix.dust, Materials.Paper));
     }
 }
