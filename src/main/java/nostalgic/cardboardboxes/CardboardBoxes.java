ffd9815fae6ed0a530ceeabf75fc5c529e1c3f9f
miles <29894500+NostalgicLorikeet@users.noreply.github.com>
2026-03-13 16:41:59 -0400

    namespace

--- src/main/java/nostalgic/cardboardboxes/CardboardBoxes.java
+++ src/main/java/nostalgic/cardboardboxes/CardboardBoxes.java
@@ -1,7 +1,10 @@
 package nostalgic.cardboardboxes;
 
+import gregtech.api.GregTechAPI;
+import gregtech.api.unification.material.event.MaterialRegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import nostalgic.cardboardboxes.client.renderer.texture.CardboardTextures;
 import nostalgic.cardboardboxes.machine.CardboardMetaTileEntities;
 import nostalgic.cardboardboxes.materials.CardboardMaterials;
@@ -18,4 +21,14 @@ public class CardboardBoxes {
         CardboardTextures.preInit();
         CardboardMaterials.register();
     }
+
+    @SubscribeEvent
+    public static void createMaterialRegistry(MaterialRegistryEvent event) {
+        GregTechAPI.materialManager.createRegistry(Tags.MOD_ID);
+    }
+
+    //@SubscribeEvent
+    //public static void registerMTERegistry(MTEManager.MTERegistryEvent event) {
+    //    GregTechAPI.mteManager.createRegistry(Tags.MOD_ID);
+    //}
 }
--- src/main/java/nostalgic/cardboardboxes/materials/CardboardMaterials.java
+++ src/main/java/nostalgic/cardboardboxes/materials/CardboardMaterials.java
@@ -1,15 +1,16 @@
 package nostalgic.cardboardboxes.materials;
 
 import gregtech.api.unification.material.Material;
+import net.minecraft.util.ResourceLocation;
+import nostalgic.gtcardboard.Tags;
 
 import static gregtech.api.unification.material.info.MaterialFlags.*;
-import static gregtech.api.util.GTUtility.gregtechId;
 
 public class CardboardMaterials {
     public static Material Cardboard;
 
     public static void register() {
-        Cardboard = new Material.Builder(17779, gregtechId("cardboard"))
+        Cardboard = new Material.Builder(17779, new ResourceLocation(Tags.MOD_ID,"cardboard"))
                 .flags(GENERATE_PLATE, DISABLE_DECOMPOSITION)
                 .color(0xA07752).build();
     }
